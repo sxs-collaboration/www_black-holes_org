01073634be497f29cf88a5e633c746b000f20cbb
Leo C. Stein <leo.stein@gmail.com>
2026-04-27 10:27:15 -0500

    Fix whitespace bug introduced in 152ebb7
    Curse you, Guido... whitespace should not be flow control!

--- _papers/inspToMd.py
+++ _papers/inspToMd.py
@@ -73,11 +73,11 @@ def insp_resp_to_md(resp, used_spec=None, used_spectre=None, summarize=True):
     if 'abstracts' in md:
         if (len(md['abstracts']) > 1):
             warn(f"More than 1 abstracts in {iid}; using first.")
-            abstract_str = md['abstracts'][0]['value']
-            abstract_str = fill(abstract_str,
-                                initial_indent='  ',
-                                subsequent_indent='  ',
-                                break_long_words=False)
+        abstract_str = md['abstracts'][0]['value']
+        abstract_str = fill(abstract_str,
+                            initial_indent='  ',
+                            subsequent_indent='  ',
+                            break_long_words=False)
     else:
         # Somehow there are INSPIRE records without abstracts... emit a warning
         # but continue with empty abstract string.

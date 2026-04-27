sxs-collaboration/www_black-holes_org

commit 152ebb7f3483fc1e5c0da729811a54607bfe5a2a

Don't break INSPIRE script on records without abstracts

_papers/inspToMd.py
@@ -70,13 +70,19 @@ def insp_resp_to_md(resp, used_spec=None, used_spectre=None, summarize=True):
         doi_str = f" \"{md['dois'][0]['value']}\""
     else:
         doi_str = ""
-    if (len(md['abstracts']) > 1):
-        warn(f"More than 1 abstracts in {iid}; using first.")
-    abstract_str = md['abstracts'][0]['value']
-    abstract_str = fill(abstract_str,
-                        initial_indent='  ',
-                        subsequent_indent='  ',
-                        break_long_words=False)
+    if 'abstracts' in md:
+        if (len(md['abstracts']) > 1):
+            warn(f"More than 1 abstracts in {iid}; using first.")
+            abstract_str = md['abstracts'][0]['value']
+            abstract_str = fill(abstract_str,
+                                initial_indent='  ',
+                                subsequent_indent='  ',
+                                break_long_words=False)
+    else:
+        # Somehow there are INSPIRE records without abstracts... emit a warning
+        # but continue with empty abstract string.
+        warn(f"No abstract for {iid}; using empty string.")
+        abstract_str = ''
     used_spec_str = " true" if used_spec else ""
     used_spectre_str = " true" if used_spectre else ""
 

commit 01073634be497f29cf88a5e633c746b000f20cbb

Fix whitespace bug introduced in 152ebb7
Curse you, Guido... whitespace should not be flow control!

_papers/inspToMd.py
@@ -73,11 +73,11 @@ def insp_resp_to_md(resp, used_spec=None, used_spectre=None, summarize=True):
     if 'abstracts' in md:
         if (len(md['abstracts']) > 1):
             warn(f"More than 1 abstracts in {iid}; using first.")
-            abstract_str = md['abstracts'][0]['value']
-            abstract_str = fill(abstract_str,
-                                initial_indent='  ',
-                                subsequent_indent='  ',
-                                break_long_words=False)
+        abstract_str = md['abstracts'][0]['value']
+        abstract_str = fill(abstract_str,
+                            initial_indent='  ',
+                            subsequent_indent='  ',
+                            break_long_words=False)
     else:
         # Somehow there are INSPIRE records without abstracts... emit a warning
         # but continue with empty abstract string.